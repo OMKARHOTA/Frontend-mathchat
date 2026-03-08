94ea3b3684b1d9b59f304d7c6c8820054b2f3a00
OMKARHOTA <97146428+OMKARHOTA@users.noreply.github.com>
2026-03-08 18:16:14 +0530

    Update agentApi.js

--- src/api/agentApi.js
+++ src/api/agentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:7860";
+const API_URL = "https://omkareswarhota1234-math.hf.space";
 
 // Send query (text OR image OR audio)
 export const sendQuery = async (query, image, audio) => {
