15ac8336544a023ae1569c6de83b5d2172061d04
Omkareswar Hota <your_email@example.com>
2026-03-08 17:35:58 +0530

    add backend

--- src/api/agentApi.js
+++ src/api/agentApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://backend:8000";
+const API_URL = "https://huggingface.co/spaces/omkareswarhota1234/Math";
 
   if (query) {
     formData.append("question", query);
